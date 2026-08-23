5e8b321d79454f973493ffc096a03653985f3817
Roshan yadav <roshanyadav@Roshans-MacBook-Air.local>
2026-08-23 12:42:12 +0530

    demo of non blocking socket

--- key-techs/io/non-blocking-io/non-blocking-io/src/main/java/io/example/Main.java
+++ key-techs/io/non-blocking-io/non-blocking-io/src/main/java/io/example/Main.java
@@ -1,17 +1,104 @@
 package io.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*
+        Non blocking server which does not wait to accept the connection
+        also does wait for incoming data on connection and removes both waiting problems
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+         * */
+
+        final int PORT = 5002;
+
+        // This is similar to ServerSocket ss =new ServerSocket()   we ask os to create a socket in listening mode
+        // which is blocking means it waits for incoming connection
+        try(ServerSocketChannel ssc = ServerSocketChannel.open()){
+
+            // Binding a address to socket
+            ssc.bind(new InetSocketAddress(PORT));
+
+            // Make it non blocking
+            ssc.configureBlocking(false);
+
+            Selector selector = Selector.open();
+
+            ssc.register(
+                    selector,
+                    SelectionKey.OP_ACCEPT
+            );
+
+            System.out.println(
+                    "server is running to PORT :"+PORT
+            );
+
+            while (true) {
+                // Blocking return only after one channel is available for the io
+                selector.select();
+
+                Iterator<SelectionKey> selectionKeyIterator = selector.selectedKeys().iterator();
+
+                while (selectionKeyIterator.hasNext()){
+
+                    SelectionKey key = selectionKeyIterator.next();
+                    selectionKeyIterator.remove();
+
+                    if(key.isAcceptable()){
+                        ServerSocketChannel ssChannel = (ServerSocketChannel) key.channel();
+                        SocketChannel client  = ssChannel.accept();
+
+                        client.configureBlocking(false);
+
+                        client.register(
+                           selector
+                          ,SelectionKey.OP_READ
+                        );
+
+                        System.out.println(
+                                "Client connected: "
+                                        + client.getRemoteAddress()
+                        );
+
+                    } else if (key.isReadable()) {
+                        SocketChannel client =
+                                (SocketChannel) key.channel();
+
+                        ByteBuffer buffer =
+                                ByteBuffer.allocate(1024);
+
+                        int bytesRead =
+                                client.read(buffer);
+
+                        if (bytesRead == -1) {
+                            client.close();
+                            continue;
+                        }
+
+                        buffer.flip();
+
+                        while (buffer.hasRemaining()) {
+                            System.out.print(
+                                    (char) buffer.get()
+                            );
+                        }
+                    }
+
+                }
+
+
+            }
+
         }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
